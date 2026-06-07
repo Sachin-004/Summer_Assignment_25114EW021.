04f848a0c5c73a0579b4d5b24862ed97fe29e77d
Sachin-004 <harysachin007@gmail.com>
2026-06-07 22:33:35 +0530

    Completed Day 7 assignment

---
+++ Day-7/Q28_RecursiveReverseNumber.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int rev = 0;
+void reverseNumber(int num) {
+    if(num==0) {
+        return;
+    }
+    rev =rev*10+num%10;
+    reverseNumber(num/10);
+}
+int main() {
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+    reverseNumber(num);
+    cout<<"Reverse Number = "<<rev<<endl;
+
+    return 0;
+}
