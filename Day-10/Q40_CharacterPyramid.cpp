12c1bc09c0ab73a237dfe25b0c55b68f215b0d7f
Sachin-004 <harysachin007@gmail.com>
2026-06-12 22:33:10 +0530

    Completed Day 10 assignments

---
+++ Day-10/Q40_CharacterPyramid.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of rows: ";
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        // Spaces
+        for(int j=1; j<=n-i; j++){
+            cout<<" ";
+        }
+        // Ascending part
+        for(int j=1; j<=i; j++){
+            cout<<(char)(j+64);
+        }
+        // Descending part
+        for(int j=i-1; j>=1; j--){
+            cout<<(char)(j+64);
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
