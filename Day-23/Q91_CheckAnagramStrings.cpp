0d5ef706c7b1b02692d921038a1c836a4d3e36de
Sachin-004 <harysachin007@gmail.com>
2026-06-27 22:17:49 +0530

    Completed Day 23 assignments

---
+++ Day-23/Q91_CheckAnagramStrings.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char str1[100], str2[100];
+    cout<<"Enter first string: ";
+    cin.getline(str1, 100);
+    cout<<"Enter second string: ";
+    cin.getline(str2, 100);
+
+    int len1 = 0, len2 = 0;
+    while (str1[len1] != '\0')
+        len1++;
+    while (str2[len2] != '\0')
+        len2++;
+    if (len1 != len2) {
+        cout<<"Strings are not anagrams."<<endl;
+        return 0;
+    }
+    for (int i = 0; i < len1; i++) {
+        int count1 = 0, count2 = 0;
+
+        for (int j = 0; j < len1; j++) {
+            if (str1[i] == str1[j])
+                count1++;
+            if (str1[i] == str2[j])
+                count2++;
+        }
+
+        if (count1 != count2) {
+            cout<<"Strings are not anagrams."<<endl;
+            return 0;
+        }
+    }
+    cout<<"Strings are anagrams."<<endl;
+
+    return 0;
+}
---
+++ Day-23/Q92_MaximumOccurringCharacter.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char str[100];
+    cout<<"Enter a string: ";
+    cin.getline(str, 100);
+    int max = 0;
+    char ch;
+    for (int i = 0; str[i] != '\0'; i++) {
+        int count = 0;
+        for (int j = 0; str[j] != '\0'; j++) {
+            if (str[i] == str[j])
+                count++;
+        }
+        if (count > max) {
+            max = count;
+            ch = str[i];
+        }
+    }
+    cout<<"Maximum occurring character: "<<ch<<endl;
+    cout<<"Frequency = "<<max<<endl;
+
+    return 0;
+}
